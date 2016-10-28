040045de2398b0745b7915a146cbf8b948bc3adb
Brian Dutz <bgdutz@gmail.com>
2016-10-28 11:53:27 -0700

    add individual question pages clickable from the question-tile

---
+++ app/components/question-detail.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    delete(question) {
+      this.sendAction('deleteQuestion', question);
+    },
+    update(question, params){
+      this.sendAction("update", question, params);
+    },
+  }
+});
