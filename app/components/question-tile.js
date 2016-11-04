34e30a0ce4a785fa73320b42ff3fad01f1542dfc
Brian Dutz <bgdutz@gmail.com>
2016-11-04 11:33:06 -0700

    add fully functional answer counter computed property, satrt working on service

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  
+
+  count: Ember.computed('question.answers.length', function(){
+    return 'Answer Count:' + " " + this.get('question.answers.length');
+  }),
 
   actions: {
     delete(question) {
