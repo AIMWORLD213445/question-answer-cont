2ba0d2bf02ee993c9e22a314526af4b26e813e2a
Brian Dutz <bgdutz@gmail.com>
2016-11-04 12:20:43 -0700

    add service function start styling

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favoriteAnswers: Ember.inject.service(),
+
+  actions: {
+    addToBestAnswers(item) {
+      this.get('favoriteAnswers').add(item);
+    },
+  }
 });
